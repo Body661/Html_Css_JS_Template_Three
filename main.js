16bddbd1ecff9ddd2940c7f291201c735e237f4c
Abdolrahman EL-hagrasy <93329344+Body661@users.noreply.github.com>
2022-03-13 21:51:47 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-let toggleMenu = document.querySelector(".toggle-menu");
-let links = document.querySelector(".nav-links");
+let megaButton = document.querySelector(".other");
+let megaMenu = document.querySelector(".mega-menu");
 
-toggleMenu.onclick = function () {
-  if (links) {
-    links.style.display == "flex"
-      ? (links.style.display = "")
-      : (links.style.display = "flex");
+megaButton.onclick = function () {
+  if (megaMenu.style.opacity == "1") {
+    megaMenu.style.opacity = 0;
+    megaMenu.style.zIndex = -1;
+    megaMenu.style.top = "calc(100% + 50px)";
+  } else {
+    megaMenu.style.opacity = 1;
+    megaMenu.style.zIndex = 100;
+    megaMenu.style.top = "calc(100% + 1px)";
+  }
+};
+
+let section = document.querySelector(".skills");
+let spans = document.querySelectorAll(".prog span");
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 200) {
+    spans.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
   }
 };
